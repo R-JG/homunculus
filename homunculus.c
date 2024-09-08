8b26e97431bb327a2e5d1a4fd386ca7d98d9392e
Robin Guerrier <robin.james.guerrier@gmail.com>
2024-09-08 03:40:08 -0400

    Begin curl integration

--- homunculus.c
+++ homunculus.c
@@ -97,25 +97,63 @@ handle_input(char keys[6])
   }
   // if (keys[0] != '\x1b') printf(keys);
   // printf("\r\n");
+
+  // send req
+
   memset(keys, '\0', 6);
+}
+
+char*
+extract_auth(char *hed_val)
+{
+  return strtok(hed_val, ";");
+}
+
+size_t
+handle_sse(char *buffer, size_t itemsize, size_t nitems, void* na)
+{
+  size_t bytes = itemsize * nitems;
+  //
+  // process json from buffer and then print the rendered output
+  //
+  printf("!!! Server Sent Event !!!");
+  printf(buffer);
+  printf("\r\n");
+  return bytes;
 }
 
 int
 main(void)
 {                          // hide cursor: "\x1b[?25l", 6    show cursor: "\x1b[?25h", 6
   char ship[] = "zod";
-  char code[] = "lidlut-tabwed-pillex-ridrup";
   char base_url[] = "http://localhost:8080";
-
-  // CURL *curl_easy_init();
-  // CURL *curl_sse = curl_easy_init();
-  // CURL *curl_req = curl_easy_init();
-  // if (!curl_sse || !curl_req) die("curl");
-  // CURLcode res;
-  // curl_easy_setopt(curl_sse, CURLOPT_URL, "https://example.com");
+  char code[28] = "lidlut-tabwed-pillex-ridrup";
+  //
+  char *test_login_url = "http://localhost:8080/~/login";
+  char *test_channel_url = "http://localhost:8080/~/channel/randomlygeneratedid";
+  char *test_password = "password=lidlut-tabwed-pillex-ridrup";
+  // curl init
+  struct curl_header *hed;
+  CURL *curl_sse = curl_easy_init();
+  CURL *curl_req = curl_easy_init();
+  if (!curl_sse || !curl_req) die("curl");
+  // curl auth
+  curl_easy_setopt(curl_req, CURLOPT_URL, test_login_url);
+  curl_easy_setopt(curl_req, CURLOPT_POSTFIELDS, test_password);
+  curl_easy_perform(curl_req);
+  curl_easy_header(curl_req, "set-cookie", 0, CURLH_HEADER, -1, &hed);
+  char *auth = extract_auth(hed->value);
+  struct curl_slist *slist = NULL;
+  // curl create sse channel
+  
+  // curl stream sse channel
+  curl_easy_setopt(curl_sse, CURLOPT_URL, test_channel_url);
+  curl_easy_setopt(curl_sse, CURLOPT_COOKIE, auth);
+  curl_easy_setopt(curl_sse, CURLOPT_WRITEFUNCTION, handle_sse);
+  curl_easy_perform(curl_sse);
   
   enable_raw_mode();
-  clear_screen();
+  // clear_screen();
   char keys[6] = {'\0'};
   while (1) {
     handle_input(keys);
